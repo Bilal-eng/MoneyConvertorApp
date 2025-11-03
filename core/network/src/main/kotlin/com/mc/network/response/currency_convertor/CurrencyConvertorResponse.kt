fbc763c3b7a1d229fca6c06cdeb514df353f2e74
BILAL HALILOGLU <bhaliloglu@tci.aero>
2025-11-03 10:32:09 +0300

    Create the data module and implement the Repository

--- core/network/src/main/kotlin/com/mc/network/response/currency_convertor/CurrencyConvertorResponse.kt
+++ core/network/src/main/kotlin/com/mc/network/response/currency_convertor/CurrencyConvertorResponse.kt
@@ -2,6 +2,7 @@ package com.mc.network.response.currency_convertor
 
 import kotlinx.serialization.Serializable
 
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
 open class CurrencyConvertorResponse {
     val message: String? = null
--- core/network/src/main/kotlin/com/mc/network/response/currency_convertor/ExchangeRatesResponse.kt
+++ core/network/src/main/kotlin/com/mc/network/response/currency_convertor/ExchangeRatesResponse.kt
@@ -1,11 +1,21 @@
 package com.mc.network.response.currency_convertor
 
 import com.mc.model.currency_convertor.CurrencyInfo
+import com.mc.model.currency_convertor.ExchangeRates
 import com.mc.model.currency_convertor.Meta
 import kotlinx.serialization.Serializable
 
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
 data class ExchangeRatesResponse(
     val meta: Meta,
     val data: Map<String, CurrencyInfo>
 )
+
+fun ExchangeRatesResponse.toExternalModel(baseCurrency: String): ExchangeRates {
+    return ExchangeRates(
+        baseCurrency = baseCurrency,
+        rates = data.mapValues { it.value.value },
+        lastUpdatedDate = meta.lastUpdateAt
+    )
+}
