f8a8087a82edbf4c7bcca9b5dff9523b0367f9c1
BILAL HALILOGLU <bhaliloglu@tci.aero>
2025-11-05 09:09:09 +0300

    Finish the UI components

---
+++ core/designssystem/src/main/kotlin/com/mc/designsystem/components/MCCard.kt
@@ -0,0 +1,58 @@
+package com.mc.designsystem.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mc.designsystem.theme.MoneyConvertorAppTheme
+
+@Composable
+fun MCCard(
+    modifier: Modifier = Modifier,
+    color: Color = Color.White,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    val shape = RoundedCornerShape(20.dp)
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .shadow(
+                elevation = 10.dp,
+                shape = shape,
+                spotColor = Color.Gray.copy(alpha = 0.5f),
+                ambientColor = Color.Gray.copy(alpha = 0.5f),
+            ),
+        shape = shape,
+        colors = CardDefaults.cardColors().copy(
+            containerColor = color
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MCCardPreview() {
+    MoneyConvertorAppTheme {
+        MCCard(
+            modifier = Modifier
+                .width(300.dp)
+                .height(200.dp)
+        ) {}
+    }
+}
